3f143121b59d9a649f9868822644ca9a8ed0b177
ValterBeff <valterbeff.2001@gmail.com>
2025-09-02 21:03:29 -0300

    Modif. Main.py

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     app.setWindowIcon(icon)
 
     # Info
-    info = Info('2.0 ^ 10.0 = 1024')
+    info = Info('')
     window.addWidgetToVLayout(info)
 
     # Display
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     window.addWidgetToVLayout(display)
     
     # Grid
-    buttonsGrid = ButtonsGrid(display)
+    buttonsGrid = ButtonsGrid(display,info, window)
     window.vLayout.addLayout(buttonsGrid)
 
     # Executa Aplicação
